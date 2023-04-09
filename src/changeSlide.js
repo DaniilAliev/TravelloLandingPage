d188e68c191bd3bde15491c057fc03d7fd9a29fa
DaniilAliev <de.aliev@yandex.ru>
2023-04-09 14:15:27 +0300

    made slider

--- src/changeSlide.js
+++ src/changeSlide.js
@@ -1,36 +1,19 @@
 const changeSlide = () => {
-    const slider = document.querySelector('.slider');
-    const sliderContent = document.querySelector('.slider-content');
-    const slides = sliderContent.children;
-    const buttons = document.querySelectorAll('.slider-buttons button');
-    let currentSlide = 0;
-    let slideInterval = setInterval(nextSlide, 5000);
+    const buttonsDiv = document.querySelector('.slider-buttons');
+    buttonsDiv.addEventListener('click', (e) => {
+        if (!e.target.classList.contains('slider-buttons')) {
+            const sliders = document.querySelector('.slider');
+        const sliderContent = document.querySelectorAll('.slider-content');
+        const buttons = document.querySelectorAll('.slider-buttons button');
 
-    function nextSlide() {
-        slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide + 1) % slides.length;
-        slides[currentSlide].classList.add('active');
-    }
-
-    function prevSlide() {
-        slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-        slides[currentSlide].classList.add('active');
-    }
-
-    buttons.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-        if (i === currentSlide) {
-            return;
+        sliderContent.forEach((slide) => slide.classList.remove('active'));
+        buttons.forEach((button) => button.classList.remove('active'));
+        e.target.classList.add('active');
+        const activeClass = e.target.classList[0];
+        const activeSlide = sliders.querySelector(`.${activeClass}`);
+        activeSlide.classList.add('active');
         }
-        slides[currentSlide].classList.remove('active');
-        currentSlide = i;
-        slides[currentSlide].classList.add('active');
-        });
-    });
-
-    slider.addEventListener('mouseover', () => clearInterval(slideInterval));
-    slider.addEventListener('mouseout', () => slideInterval = setInterval(nextSlide, 5000));
+    })
 }
 
 export default changeSlide;
